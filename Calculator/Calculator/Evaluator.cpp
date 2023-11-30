7f8f2c5ecce9c0c0c4e05c6acfc08f1f89778ca4
Alexandru Surugiu <surugiugeorge22@stud.ase.ro>
2023-11-30 21:47:00 +0200

    Varianta 1 Evaluator

---
+++ Calculator/Calculator/Evaluator.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Evaluator
+{
+	bool erori = false;
+	double rezultat = 0.00;
+
+public:
+
+	bool getErori()
+	{
+		return this->erori;
+	}
+
+	void setErori(bool eroare)
+	{
+		this->erori = eroare;
+	}
+
+	double Operatii(double operand1, double operand2, char semn)
+	{
+		try
+		{
+			switch (semn)
+			{
+				case '+':
+					rezultat = operand1 + operand2;
+					break;
+
+				case '-':
+					rezultat = operand1 - operand2;
+					break;
+
+				case '*':
+					rezultat = operand1 * operand2;
+					break;
+
+				case '/':
+					if (operand2 == 0)
+						throw exception("Eroare: incerci sa imparti un numar la 0!");
+					else
+						rezultat = operand1 / operand2;
+					break;
+
+				default:
+					cout << "\n" << "Semnul introdus este necunoscut (inca)!";
+					this->erori = true;
+					break;
+			}
+		}
+
+		catch (exception e)
+		{
+			cout << "\n" << e.what();
+		}
+	}
+
+
+
+};

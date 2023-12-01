e439be30de9bcfa5668d8a0dbe5317ecc286044f
Alexandru Surugiu <surugiugeorge22@stud.ase.ro>
2023-12-02 00:39:57 +0200

    Verificare2 (Solutie temporara pentru Spatii)

--- Calculator/Calculator/Verificare.cpp
+++ Calculator/Calculator/Verificare.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 //#include <Evaluator.cpp>
 
 using namespace std;
@@ -36,5 +38,24 @@ public:
 		}
 	}
 
+	void Spatii(const string& expresie, vector<string>& sir) 
+	{
+		size_t start = 0;
+		char operand;
 
+		for (size_t i = 0; i < expresie.size(); i++) 
+		{
+			if (isdigit(expresie[i] == false) && expresie[i] != '.') 
+			{
+				operand = expresie[i];
+				sir.push_back(expresie.substr(start, i - start));
+				sir.push_back(string(1, operand));
+				start = i + 1;
+			}
+		}
+
+		if (start < expresie.size()) {
+			sir.push_back(expresie.substr(start));
+		}
+	}
 };
